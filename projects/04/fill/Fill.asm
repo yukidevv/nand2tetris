43bf6df92ff6455104f01a121e0699782c27bc4c
yukidevv <nomad048@gmail.com>
2023-12-27 21:13:36 +0900

    completed 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,49 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+  @i //Counter
+  M=0
+  @KBD
+  D=M
+
+  @BLACK_FILL
+  D;JNE // !=0 キーボード入力があればBLACK_FILLへ
+  @WHITE_FILL
+  D;JEQ// =0 キーボード入力なければWHITE_FILLへ
+
+(BLACK_FILL)
+  @SCREEN
+  D=A
+
+  @i
+  A=D+M //0x16384~
+  M=-1 //0xffff(1の補数)
+
+  @i
+  M=M+1
+  D=M
+  @8192
+  D=D-A //i-8192
+  @BLACK_FILL
+  D;JLT // <0
+  @LOOP
+  0;JMP
+
+(WHITE_FILL)
+  @SCREEN
+  D=A
+
+  @i
+  A=D+M
+  M=0 //0x0000
+
+  @i
+  M=M+1
+  D=M
+  @8192
+  D=D-A //i-8192
+  @WHITE_FILL
+  D;JLT // <0
+  @LOOP
+  0;JMP
